90bea7feb277a78ddf055d395e2dff6e76478079
Patrick A Wallace <pawrsccouk@googlemail.com>
2015-03-30 20:10:09 +0100

    Initial Commit

---
+++ Fortune/FortuneView.h
@@ -0,0 +1,13 @@
+//
+//  FortuneView.h
+//  Fortune
+//
+//  Created by Patrick Wallace on 30/03/2015.
+//  Copyright (c) 2015 Patrick Wallace. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface FortuneView : ScreenSaverView
+
+@end
